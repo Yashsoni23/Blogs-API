221ed276c5710552958b7d1c20d7e11830fd2089
Yash simejiya <107744977+Yashsoni23@users.noreply.github.com>
2022-11-25 17:49:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Blog = require("./api/Blog");
-const cors = require("cors");
 const connectionString = process.env.DBSTRING;
 mongoose.connect(connectionString).then((result => console.log("connected to mongoDB"))).catch(err => console.log(err))
-const router = express.Router();
+const app = express.Router();
 
-router.use(cors);
-router.use("/blogs", Blog);
+app.use("/blogs", Blog);
 
-module.exports = router;
+module.exports = app;
